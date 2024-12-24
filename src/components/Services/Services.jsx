b339ef76d3f9357b81b86567b3187d430e340835
Ashan Vimukthi <ashanwithanarachchi@gmail.com>
2024-12-24 21:53:49 +0530

    Add services page UI to the appliation

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,13 +1,119 @@
 import React from 'react'
+import './Services.css';
 import NavBar from '../NavBar/NavBar';
+import Footer from '../Footer/Footer';
+import { Link } from 'react-router-dom';
+
+import ServiceBanner from '../../assets/about-banner.svg'
+import { FaPlaneDeparture } from "react-icons/fa";
+import { FaHotel, FaTaxi , FaPersonHiking } from "react-icons/fa6";
+import { GiMeditation, GiForestCamp, GiElephant } from "react-icons/gi";
+
 
 function Services() {
+  const serviceItems = [
+    {
+
+    }
+  ]
+
+
+
   return (
     <>
       <header>
         <NavBar/>
       </header>
-      <div className="w-full bg-primary">Services</div>
+
+      <main className="mt-10 text-mainfont">
+        {/* heder section */}
+        <section className="w-5/6 lg:w-4/5 mx-auto flex flex-col gap-8 sm:gap-5 sm:flex-row lg:gap-10 lg:flex-row">
+          <section className="sm:w-1/2 lg:w-2/5 lg:pt-10">
+            <h1 className="mb-3 text-3xl lg:text-6xl font-bold lg:mb-5">Servic<span className="text-secondary">es</span></h1>
+            <p className="text-sm w-full sm:text-sm sm:pr-5  lg:text-lg font-semibold text-[#555758] lg:pr-16">
+              We offers a wide range of personalized services designed to make your travel experience seamless and memorable. Whether you're looking for adventure, relaxation, or cultural exploration, our team is dedicated to providing exceptional service and support throughout your journey.
+            </p>
+          </section>
+          <section  className="sm:w-1/2 lg:w-3/5">
+              <img src={ServiceBanner} alt="services banner image" className="max-h-48 sm:max-h-60 w-full lg:max-h-96"/>
+          </section>
+        </section>
+
+        {/* categories section */}
+        <div className="mt-28">
+          <div className="py-2 text-center mb-4">
+            <h1 className="text-lg font-bold lg:text-4xl">Categories</h1>
+            <p className="text-[#7B7B7B] font-medium text-lg">our unique values for you</p>
+          </div>
+
+          <div className="text-mainfont">
+            <div className="w-4/5 mx-auto mb-3">
+              <section className="w-full flex  justify-center items-start gap-10 flex-wrap">
+                {/* to do: link set with relavent page */}
+                <Link to="/add link">
+                  <article className="bg-backgbround  shadow-card mt-3 w-[17rem] min-h-40 lg:min-h-36 rounded-lg flex flex-col justify-center items-center text-center hover:cursor-pointer card-hover">
+                    <FaPlaneDeparture className="w-12 h-12 text-primary"/>
+                    <p className="mt-4 font-bold text-sm">Tourist Visa <br/> Applying</p>
+                  </article>
+                </Link>
+
+                {/* to do: link set with relavent page */}
+                <Link to="/add link">
+                  <article className="bg-primary  shadow-card mt-3 w-[17rem] min-h-40 lg:min-h-36 rounded-lg flex flex-col justify-center items-center text-center card-hover">
+                    <GiMeditation className="w-12 h-12 text-secondary"/>
+                    <p className="mt-4 font-bold text-sm text-backgbround">Wellness  <br/> Services</p>
+                  </article>
+                </Link>
+
+                {/* to do: link set with relavent page */}
+                <Link to="/add link">
+                  <article className="bg-backgbround  shadow-card mt-3  w-[17rem] min-h-40 lg:min-h-36 rounded-lg flex flex-col justify-center items-center text-center hover:cursor-pointer card-hover">
+                    <FaHotel className="w-12 h-12 text-primary"/>
+                    <p className="mt-4 font-bold text-sm">Hotel <br/> Booking</p>
+                  </article>
+                </Link>
+
+                {/* to do: link set with relavent page */}
+                <Link to="/add link">
+                  <article className="bg-backgbround  shadow-card mt-3 w-[17rem] min-h-40 lg:min-h-36 rounded-lg flex flex-col justify-center items-center text-center card-hover">
+                    <FaPersonHiking className="w-12 h-12 text-primary"/>
+                    <p className="mt-4 font-bold text-sm">Tourist Guider <br/> Booking</p>
+                  </article>
+                </Link>
+
+                {/* to do: link set with relavent page */}
+                <Link to="/add link">
+                  <article className="bg-backgbround  shadow-card mt-3 w-[17rem] min-h-40 lg:min-h-36 rounded-lg flex flex-col justify-center items-center text-center card-hover">
+                    <FaTaxi className="w-12 h-12 text-primary"/>
+                    <p className="mt-4 font-bold text-sm">Transport  <br/> Booking</p>
+                  </article>
+                </Link>
+
+                {/* to do: link set with relavent page */}
+                <Link to="/add link">
+                  <article className="bg-backgbround  shadow-card mt-3 w-[17rem] min-h-40 lg:min-h-36 rounded-lg flex flex-col justify-center items-center text-center card-hover">
+                    <GiForestCamp className="w-12 h-12 text-primary"/>
+                    <p className="mt-4 font-bold text-sm">Transport  <br/> Booking</p>
+                  </article>
+                </Link>
+
+                {/* to do: link set with relavent page */}
+                <Link to="/add link">
+                  <article className="bg-backgbround  shadow-card mt-3 w-[17rem] min-h-40 lg:min-h-36 rounded-lg flex flex-col justify-center items-center text-center card-hover">
+                    <GiElephant className="w-12 h-12 text-primary"/>
+                    <p className="mt-4 font-bold text-sm">Safari  <br/> Services</p>
+                  </article>
+                </Link>              
+              </section>             
+            </div>
+          </div>
+        </div>
+      </main>
+
+      {/* footer section */}
+      <div className="bg-primary mt-40">
+        <Footer />
+      </div>
     </>
    
   )
